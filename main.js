72906f3a335c9a5e12cd1247931ffe976fe22f26
wangliuyong <1355498705@qq.com>
2018-08-14 14:26:04 +0800

    使用JQuery的AJAX

--- main.js
+++ main.js
@@ -1,47 +1,17 @@
-//封装一个AJAX
-window.jquery={}
-window.$=window.jquery
-jquery.ajax=function(options){
-    return new Promise(function(resolve,reject){
-        var request=new XMLHttpRequest();
-        let {url,method,body,headers}=options
-        console.log(method)
-        request.open(method,url); 
-        for(let key in headers){
-            let value =headers[key]
-            request.setRequestHeader(key,value)
-        }
-        request.send(body);
-        
-        request.onreadystatechange=function(){
-            
-            if(request.readyState==4){
-                if(request.status < 400){
-                    resolve.call(undefined,request.responseText)
-                }else{
-                    reject.call(undefined,request)
-                }
-            }
-        }
-    
-    })
-}
-
-
-let button=document.getElementById("button");
-let number = document.getElementById('number');
-
 button.addEventListener('click',()=>{
-    let a=$.ajax({
-        'method':'post',
+    $.ajax({
+        'method':'GET',
         'url':'./x',
-        'body':'"wang":"15"',
         'headers':{
             'name':'wang',
             'Content-Type':'application/x-www-form-urlencoded'
         }
     }).then(
-        (Text)=>{console.log(Text)},
-        (text)=>{console.log(text.responseText)}
+        (responseText)=>{
+            console.log(responseText);//jquery会根据content-type智能的将响应的字符串转成对应的对象
+        },
+        (request)=>{
+            console.log(request);
+        }
     )
-});
+})
